6d9391752d684039b568fcaa010770d10dd3030a
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-03-25 23:41:06 +0000

    websockets config with socket.io

--- gp25_e/index.js
+++ gp25_e/index.js
@@ -3,9 +3,20 @@ require('dotenv').config(); // Load .env variables
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+// WS setup
+const http = require('http');
+const {Server} = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+const httpSV = http.createServer(app);
+const io = new Server(httpSV, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
 
 app.use(cors());
 app.use(express.json());
@@ -26,6 +37,15 @@ db.connect(err => {
   console.log('Connected to database');
 });
 
+//Socket io connection handler
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
 // GET /users
 app.get('/users', (req, res) => {
   db.query('SELECT id, username, email, created_at FROM users', (err, results) => {
@@ -33,10 +53,13 @@ app.get('/users', (req, res) => {
       console.error('Error fetching users:', err);
       return res.status(500).json({ error: 'Database error' });
     }
+
+    io.emit('users_updated', results)
+
     res.json(results);
   });
 });
 
-app.listen(port, () => {
+httpSV.listen(port, () => {
   console.log(`API running on port ${port}`);
 });
