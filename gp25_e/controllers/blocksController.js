e0d8309099234fe7110aef9c08892cb467d44354
MEGalamba <mariaeduardagalamba@gmail.com>
2025-06-07 20:49:56 +0100

    feat: added logic to fetch blocks in a time inetrval

---
+++ gp25_e/controllers/blocksController.js
@@ -0,0 +1,27 @@
+blocksControllers
+
+const db = require('../models/db');
+
+// Get all blocks that overlap within an interval
+exports.getOverlappingBlocks = async (req, res) => {
+  const { start, end } = req.query;
+
+  if (!start || !end ) {
+    return res.status(400).json({ error: 'Missing query parameters' });
+  }
+
+  try {
+    const [blocks] = await db.query(`
+      SELECT *
+      FROM Block
+      WHERE  
+        StartHour < ? AND 
+        EndHour > ?
+    `, [end, start]);
+
+    res.json(blocks);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  }
+};
